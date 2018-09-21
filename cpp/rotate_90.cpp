999d60fe1941fff18c3fdde2f57e39f964b70b70
sisansahu1 <32607653+sisansahu1@users.noreply.github.com>
2018-09-21 18:54:31 +0530

    Create rotate_90.cpp

---
+++ cpp/rotate_90.cpp
@@ -0,0 +1,36 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[20][20],b[20][20],i,j,n,k;
+	cout<<"Enter the value of n";
+	cin>>n;
+	cout<<endl<<"Enter the elements"<<endl;
+	for(i=1;i<=n;i++)
+		for(j=1;j<=n;j++)
+			cin>>a[i][j];
+			
+	k=n;
+	
+	cout<<"After Rotate of 90 degree"<<endl;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+			{
+			b[k][i]=a[i][j];
+			k--;
+			}
+		k=n;
+	}
+	
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			cout<<b[i][j];
+		}
+	cout<<endl;
+	}
+	
+}
